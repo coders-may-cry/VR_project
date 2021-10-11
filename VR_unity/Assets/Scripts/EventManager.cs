b2a9ea77ce70a4261520e8ad0f13d93ac04b0601
valerrkaaa <val_g_2001@bk.ru>
2021-10-11 22:07:05 +0500

    Created main folders

---
+++ VR_unity/Assets/Scripts/EventManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    /*
+     * 
+     * Этот скрипт собирает глобальную информацию о событиях на сцене, запускает сценарные скрипты и т.д.
+     * 
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
